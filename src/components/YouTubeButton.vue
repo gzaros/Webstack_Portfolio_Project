131151502aa3f4d4550e0449ab8ab7b908c16a11
gzaros <templatewebvo@gmail.com>
2024-05-17 15:35:26 +0100

    Webstack_Portfolio_Project

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 text-white border-gray-600 bg-gray-500 hover:bg-gray-600 transition-colors"
+  >
+    <slot>Details</slot>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
